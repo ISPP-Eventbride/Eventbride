59b9a89ab6818b83c2509e58e7a9d621d58a24d0
Gon <gonnavrem@alum.us.es>
2025-04-24 18:52:43 +0200

    fix: security routes typo
    No desc needed. #457

--- src/main/java/com/eventbride/config/SecurityConfig.java
+++ src/main/java/com/eventbride/config/SecurityConfig.java
@@ -100,8 +100,8 @@ public class SecurityConfig {
                                                                 "/api/invitation/**",
                                                                 "/api/users/getAdmin",
                                                                 "/api/invitation/create/**",
-                                                                "/api/ratings//other-service/{id}",
-                                                                "/api/ratings//venue/{id}",
+                                                                "/api/ratings/other-service/{id}",
+                                                                "/api/ratings/venue/{id}",
                                                                 "/api/ratings/average/other-service/{id}",
                                                                 "/api/ratings/average/venue/{id}",
                                                                 "/api/ratings/service/{id}/isVoted/{userId}")
